e64adeeef32f43386a6c99b7501964784f67697e
irakli2206 <iraklibego1@gmail.com>
2024-10-02 16:47:21 +0400

    first two tasks done

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -6,6 +6,7 @@ import {
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
+  ArchiveBoxXMarkIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
@@ -99,6 +100,22 @@ export default function EditInvoiceForm({
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
+              <div className="flex items-center">
+                <input
+                  id="canceled"
+                  name="status"
+                  type="radio"
+                  value="canceled"
+                  defaultChecked={invoice.status === 'canceled'}
+                  className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                />
+                <label
+                  htmlFor="canceled"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+                >
+                  Canceled <ArchiveBoxXMarkIcon className="h-4 w-4" />
+                </label>
+              </div>
               <div className="flex items-center">
                 <input
                   id="pending"
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,20 +1,30 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon, ArchiveBoxXMarkIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
+import moment from 'moment'
 
-export default function InvoiceStatus({ status }: { status: string }) {
+export default function InvoiceStatus({ status, date }: { status: string, date: string }) {
+  const OVERDUE_DAYS = 14
+  const isOverdue = moment().diff(moment(date), 'days') > OVERDUE_DAYS;
+
   return (
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-gray-100 text-gray-500': status === 'pending' || status === 'canceled',
           'bg-green-500 text-white': status === 'paid',
         },
       )}
     >
+      {status === 'canceled' ? (
+        <>
+          Canceled
+          <ArchiveBoxXMarkIcon className="ml-1 w-4 text-gray-500" />
+        </>
+      ) : null}
       {status === 'pending' ? (
         <>
-          Pending
+          {isOverdue ? 'Overdue' : 'Pending'}
           <ClockIcon className="ml-1 w-4 text-gray-500" />
         </>
       ) : null}
